54482c489650b9d7a5ff1cfaa7e8aa879e7a18b0
kalvish <kalan@Kalans-Mac-mini.local>
2014-11-20 20:01:29 +0800

    little changes

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,4 +1,18 @@
+import java.util.Arrays;
+
 
 public class QuickSort {
+	public Comparable [] sort(Comparable [] heights) {
+		// TODO Auto-generated method stub
+		Comparable [] heightsEdit = Arrays.copyOf(heights, heights.length);
 
+		long startTime = System.nanoTime();
+		// ... the code being measured ...
+		
+		long estimatedTime = System.nanoTime() - startTime;
+
+		System.out.println("\n" + this.getClass().getSimpleName() + " : " + estimatedTime);
+
+		return heightsEdit;
+	}
 }
