630e42f1ec9f9c8720510426cf3e989f443d5ece
kalvish <kalanvishwanath@gmail.com>
2014-11-23 14:31:38 +0800

    Added 2 arrays merging method

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -15,4 +15,50 @@ public class MergeSort {
 
 		return heightsEdit;
 	}
+	
+	public Comparable [] compareAndMergeTwoSortedArrays(Comparable [] mergedArray, Comparable [] a, Comparable [] b) {
+		int comparisons = 0;
+		int a_inc =0, b_inc = 0, merge_inc = 0;
+		while (a_inc<a.length && b_inc<b.length) {
+			
+			if(Utils.less(a[a_inc], b[b_inc])){
+				comparisons++;
+				mergedArray[merge_inc] = a[a_inc];
+				a_inc++;
+			}else if(Utils.less(b[b_inc], a[a_inc])){
+				comparisons++;
+				mergedArray[merge_inc] = b[b_inc];
+				b_inc++;
+			}else{
+				comparisons++;
+				mergedArray[merge_inc] = a[a_inc];
+				merge_inc++;
+				mergedArray[merge_inc] = a[a_inc];
+				a_inc++;
+				b_inc++;
+			}
+			merge_inc++;
+			
+		}
+		
+		while (a_inc==a.length && b_inc<b.length) {
+			mergedArray[merge_inc] = b[b_inc];
+			b_inc++;
+			merge_inc++;
+		}
+		while (b_inc==b.length && a_inc<a.length) {
+			mergedArray[merge_inc] = a[a_inc];
+			a_inc++;
+			merge_inc++;
+		}
+//		if(a_inc==a.length && b_inc==b.length-1){
+//			mergedArray[merge_inc] = b[b_inc];
+//		}else if(b_inc==b.length && a_inc==a.length-1){
+//			mergedArray[merge_inc] = a[a_inc];
+//		}
+		System.out.println(Integer.toString(comparisons) +"\n");
+		System.out.println(Arrays.toString(mergedArray));
+		return mergedArray;
+	}
+	
 }
--- src/Problem1.java
+++ src/Problem1.java
@@ -15,29 +15,35 @@ public class Problem1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Double[] heights = {5.45,3.56,6.45,2.45,6.34,1.45};
-		Double[] heights = new Double[1000];
-		LinkedList<Double> heightsLinkedList = new LinkedList<>();
-		
-		for (int i = 0; i < heights.length; i++) {
-			heights[i] = Math.random();
-			heightsLinkedList.add(heights[i]);
-		}
-		
-		BubbleSort bubbleSort = new BubbleSort();
-		Comparable [] sortedArray = bubbleSort.sort(heights);
-		System.out.println(Arrays.toString(sortedArray));
-		
-		SelectionSort selectionSort = new SelectionSort();
-		Comparable [] selectionSortedArray = selectionSort.sort(heights);
-		System.out.println(Arrays.toString(selectionSortedArray));
-		
-		InsertionSort insertionSort = new InsertionSort();
-		Comparable [] sortedDoubles = insertionSort.sort(heights);
-		System.out.println(Arrays.toString(sortedDoubles));
-		
-		LinkedList<Double> sortedLS = insertionSort.sort(heightsLinkedList);
+//		Double[] heights = new Double[1000];
+//		LinkedList<Double> heightsLinkedList = new LinkedList<>();
+//		
+//		for (int i = 0; i < heights.length; i++) {
+//			heights[i] = Math.random();
+//			heightsLinkedList.add(heights[i]);
+//		}
+//		
+//		BubbleSort bubbleSort = new BubbleSort();
+//		Comparable [] sortedArray = bubbleSort.sort(heights);
+//		System.out.println(Arrays.toString(sortedArray));
+//		
+//		SelectionSort selectionSort = new SelectionSort();
+//		Comparable [] selectionSortedArray = selectionSort.sort(heights);
+//		System.out.println(Arrays.toString(selectionSortedArray));
+//		
+//		InsertionSort insertionSort = new InsertionSort();
+//		Comparable [] sortedDoubles = insertionSort.sort(heights);
+//		System.out.println(Arrays.toString(sortedDoubles));
+//		
+//		LinkedList<Double> sortedLS = insertionSort.sort(heightsLinkedList);
 		
 		//System.out.println(sortedLS.toString());
+		
+		Integer[] a = {2,4,6,8,10};
+		Integer[]b = {1,3,5,7,9};
+		Integer[] merge = new Integer[a.length+b.length];
+		MergeSort mergeSort = new MergeSort();
+		mergeSort.compareAndMergeTwoSortedArrays(merge, a, b);
 	}
 
 }
