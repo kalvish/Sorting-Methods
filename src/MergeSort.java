932ae6a00c79411c712f6c8c727c6dbd796c5c7d
kalvish <kalanvishwanath@gmail.com>
2014-11-24 02:23:40 +0800

    Improved  merging method

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -5,15 +5,89 @@ public class MergeSort {
 	public Comparable [] sort(Comparable [] heights) {
 		// TODO Auto-generated method stub
 		Comparable [] heightsEdit = Arrays.copyOf(heights, heights.length);
+		Comparable [] mergedArray = new Comparable [heightsEdit.length];
 
 		long startTime = System.nanoTime();
 		// ... the code being measured ...
+		this.compareAndMergeTwoSortedArrays(mergedArray, heightsEdit,0, 1);
+		this.compareAndMergeTwoSortedArrays(mergedArray, heightsEdit,2, 3);
+		
+		this.compareAndMergeTwoSortedArrays(mergedArray, mergedArray,0, 3);
+		
+		
+		this.compareAndMergeTwoSortedArrays(mergedArray, heightsEdit,4, 5);
+		this.compareAndMergeTwoSortedArrays(mergedArray, heightsEdit,6, 7);
+		
+		this.compareAndMergeTwoSortedArrays(mergedArray, mergedArray,4, 7);
+		
+		
+		this.compareAndMergeTwoSortedArrays(mergedArray, mergedArray,0, 7);
 		
 		long estimatedTime = System.nanoTime() - startTime;
 
 		System.out.println("\n" + this.getClass().getSimpleName() + " : " + estimatedTime);
 
 		return heightsEdit;
+	}
+	
+	
+//	private Comparable [] mergeSort(Comparable[] toSort, int i, int j){
+//		if((j-i+1)<=2){
+//			this.mergeSort(toSort);
+//		}
+//		return null;
+//	}
+//	
+//	void play (){
+//		this.compareAndMergeTwoSortedArrays(mergedArray, toSort, b)
+//	}
+	
+	public Comparable [] compareAndMergeTwoSortedArrays(Comparable [] mergedArray, Comparable [] toSort, int startIndex, int endIndex) {
+		int inter =startIndex+((endIndex-startIndex+1)/2);
+		Comparable [] a = Arrays.copyOfRange(toSort, startIndex, inter);
+		Comparable [] b = Arrays.copyOfRange(toSort, inter, endIndex+1);
+		int comparisons = 0;
+		int a_inc =0, b_inc = 0, merge_inc = startIndex;
+		while (a_inc<a.length && b_inc<b.length) {
+			
+			if(Utils.less(a[a_inc], b[b_inc])){
+				comparisons++;
+				mergedArray[merge_inc] = a[a_inc];
+				a_inc++;
+			}else if(Utils.less(b[b_inc], a[a_inc])){
+				comparisons++;
+				mergedArray[merge_inc] = b[b_inc];
+				b_inc++;
+			}else{
+				comparisons++;
+				mergedArray[merge_inc] = a[a_inc];
+				merge_inc++;
+				mergedArray[merge_inc] = a[a_inc];
+				a_inc++;
+				b_inc++;
+			}
+			merge_inc++;
+			
+		}
+		
+		while (a_inc==a.length && b_inc<b.length) {
+			mergedArray[merge_inc] = b[b_inc];
+			b_inc++;
+			merge_inc++;
+		}
+		while (b_inc==b.length && a_inc<a.length) {
+			mergedArray[merge_inc] = a[a_inc];
+			a_inc++;
+			merge_inc++;
+		}
+//		if(a_inc==a.length && b_inc==b.length-1){
+//			mergedArray[merge_inc] = b[b_inc];
+//		}else if(b_inc==b.length && a_inc==a.length-1){
+//			mergedArray[merge_inc] = a[a_inc];
+//		}
+	//	System.out.println(Integer.toString(comparisons) +"\n");
+	//	System.out.println(Arrays.toString(mergedArray));
+		return mergedArray;
 	}
 	
 	public Comparable [] compareAndMergeTwoSortedArrays(Comparable [] mergedArray, Comparable [] a, Comparable [] b) {
--- src/Problem1.java
+++ src/Problem1.java
@@ -14,7 +14,7 @@ public class Problem1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Double[] heights = {5.45,3.56,6.45,2.45,6.34,1.45};
+		Double[] heights = {5.45,3.56,6.45,2.45,6.34,1.45,4.35,1.34};
 //		Double[] heights = new Double[1000];
 //		LinkedList<Double> heightsLinkedList = new LinkedList<>();
 //		
@@ -23,27 +23,29 @@ public class Problem1 {
 //			heightsLinkedList.add(heights[i]);
 //		}
 //		
-//		BubbleSort bubbleSort = new BubbleSort();
-//		Comparable [] sortedArray = bubbleSort.sort(heights);
-//		System.out.println(Arrays.toString(sortedArray));
+		BubbleSort bubbleSort = new BubbleSort();
+		Comparable [] sortedArray = bubbleSort.sort(heights);
+		//System.out.println(Arrays.toString(sortedArray));
 //		
-//		SelectionSort selectionSort = new SelectionSort();
-//		Comparable [] selectionSortedArray = selectionSort.sort(heights);
-//		System.out.println(Arrays.toString(selectionSortedArray));
-//		
-//		InsertionSort insertionSort = new InsertionSort();
-//		Comparable [] sortedDoubles = insertionSort.sort(heights);
-//		System.out.println(Arrays.toString(sortedDoubles));
+		SelectionSort selectionSort = new SelectionSort();
+		Comparable [] selectionSortedArray = selectionSort.sort(heights);
+		//System.out.println(Arrays.toString(selectionSortedArray));
+		
+		InsertionSort insertionSort = new InsertionSort();
+		Comparable [] sortedDoubles = insertionSort.sort(heights);
+		//System.out.println(Arrays.toString(sortedDoubles));
 //		
 //		LinkedList<Double> sortedLS = insertionSort.sort(heightsLinkedList);
 		
 		//System.out.println(sortedLS.toString());
 		
-		Integer[] a = {2,4,6,8,10};
-		Integer[]b = {1,3,5,7,9};
+		Integer[] a = {2,9,6,8,10,12,4,2};
+		Integer[]b = {2,4,1,3,9};
 		Integer[] merge = new Integer[a.length+b.length];
 		MergeSort mergeSort = new MergeSort();
-		mergeSort.compareAndMergeTwoSortedArrays(merge, a, b);
+		mergeSort.sort(heights);
+		//mergeSort.compareAndMergeTwoSortedArrays(merge, a, b);
+		//mergeSort.compareAndMergeTwoSortedArrays(merge, b,1, 2);
 	}
 
 }
